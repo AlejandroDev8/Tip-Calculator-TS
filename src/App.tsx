2134b4439f2723506dcdf40d3de8c7c3ad09d854
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-14 01:35:14 -0600

    Implement MenuItems component and integrate it into the main app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { MenuItems } from "./components/MenuItems"
+import { menuItems } from "./data/database"
+
 export const App = () => {
+
   return (
+    <>
     <header className="bg-sky-400 py-5">
       <h1 className="text-center text-4xl font-black text-white">
         Calculadora de Propinas y consumo
       </h1>
     </header>
+
+    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 gap-4">
+      <div>
+        <h2>Menú</h2>
+        {menuItems.map(item => (
+          <MenuItems key={item.id}/>
+        ))}
+      </div>
+      <div>
+        <h2>Consumo</h2>
+      </div>
+    </main>
+    </>
   )
 }
