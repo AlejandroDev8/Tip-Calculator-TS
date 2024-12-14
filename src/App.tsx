ea2310fd3345cd667601a22727825d55a3cb8a23
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-14 01:04:03 -0600

    Update header to display title for tip calculator app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 export const App = () => {
   return (
-    <div>App</div>
+    <header className="bg-sky-400 py-5">
+      <h1 className="text-center text-4xl font-black text-white">
+        Calculadora de Propinas y consumo
+      </h1>
+    </header>
   )
 }
