6d53b308ef0714f77d9f166a761d91d27ea15f77
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-14 20:07:19 -0600

    Refactor addItem function in UseOrder hook to initialize quantity for new items

--- src/hooks/UseOrder.ts
+++ src/hooks/UseOrder.ts
@@ -6,8 +6,13 @@ export default function UseOrder() {
   const [order, setOrder] = useState<OrderItemType[]>([])
 
   const addItem = (item: MenuItemsType) => {
-    console.log(item)
+    
+    const newItem: OrderItemType = {...item, quantity: 1}
+
+    setOrder([...order, newItem])
   }
+
+  console.log(order)
 
   return {
     addItem
