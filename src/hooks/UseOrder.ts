2ae6fbcb05388c8ca78fdf8dc75a5ff1068ff15b
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-14 20:47:43 -0600

    Improve addItem function in UseOrder hook to handle existing items by updating quantity

--- src/hooks/UseOrder.ts
+++ src/hooks/UseOrder.ts
@@ -6,10 +6,16 @@ export default function UseOrder() {
   const [order, setOrder] = useState<OrderItemType[]>([])
 
   const addItem = (item: MenuItemsType) => {
-    
-    const newItem: OrderItemType = {...item, quantity: 1}
 
-    setOrder([...order, newItem])
+    const intemExist = order.find(orderItem => orderItem.id === item.id)
+
+    if(intemExist) {
+      const updatedOrder = order.map(orderItem => orderItem.id === item.id ? {...orderItem, quantity: orderItem.quantity + 1} : orderItem)
+      setOrder(updatedOrder)
+    } else {
+      const newItem: OrderItemType = {...item, quantity: 1}
+      setOrder([...order, newItem])
+    }
   }
 
   console.log(order)
