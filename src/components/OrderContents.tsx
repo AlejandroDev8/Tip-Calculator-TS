0aa6b46eb1c2e8178c47a5645cd42ff2a46603a2
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-12-15 12:45:53 -0600

    Enhance OrderContents component to format item prices and display total cost for each item

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItemType } from "../types"
 
 type OrderContentsProps = {
@@ -8,13 +9,21 @@ export const OrderContents = ({order} : OrderContentsProps) => {
   return (
     <div>
       <h2 className="text-4xl font-black">Consumo</h2>
-      <div className="space-y-3 mt-5">
+      <div className="space-y-3 mt-10">
         {order.length === 0 ? <p className="text-center">No hay ordenes actualmente</p> : 
           (order.map(item =>(
-            <div key={item.id} className="flex justify-between">
-                <p>
-                  {item.name} - {item.quantity} - {item.price}
+            <div key={item.id} className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b">
+                <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
                 </p>
+                <p className="font-black">
+                  Cantidad: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                </p>
+                </div>
+                <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black hover:bg-red-700">
+                  X
+                </button>
             </div>
           )))}
       </div>
